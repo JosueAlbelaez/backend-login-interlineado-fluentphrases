913ea44fb7f6219ea4b64bc400944a3857a13dbe
josue <josuealbelaez@gmail.com>
2025-03-02 11:09:16 -0300

    #3

--- src/server.ts
+++ src/server.ts
@@ -55,15 +55,18 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:8080',
+    'https://interlineado.fluentphrases.org',
     'https://fluentphrases.org',
     'https://interlineado-backend-fluent-phrases.vercel.app',
     'https://backend-interlineado.vercel.app',
-    'https://interlineado.fluentphrases.org',
+
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.options('*', cors());
 
 app.use(express.json());
 
